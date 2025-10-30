0aff8f5195e7c1b65c7e038788ff5202d7f06803
jack-url <jackkiriga@.com>
2025-10-30 13:01:30 +0300

    db setup

---
+++ database/db.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql" // MySQL driver
+)
+
+var DB *sql.DB
+
+// Connect initializes and verifies the MySQL connection
+func Connect() {
+	var err error
+
+	// credentials
+
+	username := "root"
+	password := "Born2001$"
+	host := "127.0.0.1"
+	port := "3306"
+	dbname := "hardware_shop"
+
+	// Build the connection string
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, host, port, dbname)
+
+	// Open a connection
+
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf(" Error opening MySQL connection: %v", err)
+	}
+
+	// Test the connection
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf(" Cannot connect to MySQL: %v", err)
+	}
+
+	log.Println(" Connected to MySQL database successfully!")
+}
